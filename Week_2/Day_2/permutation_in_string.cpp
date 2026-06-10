eb5d2764cacb3cf82951953e495d884c6e6d0f00
Daksh Bhasin <divyanshmaurya10001@gmail.com>
2026-06-10 10:37:28 +0530

    Week 2 Day 2: Permutation in String

---
+++ Week_2/Day_2/permutation_in_string.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool checkInclusion(string s1, string s2) {
+
+    int n = s1.length();
+    int m = s2.length();
+
+    if (n > m) {
+        return false;
+    }
+
+    vector<int> freq1(26, 0);
+    vector<int> freq2(26, 0);
+
+    for (int i = 0; i < n; i++) {
+        freq1[s1[i] - 'a']++;
+        freq2[s2[i] - 'a']++;
+    }
+
+    if (freq1 == freq2) {
+        return true;
+    }
+
+    for (int i = n; i < m; i++) {
+
+        freq2[s2[i] - 'a']++;
+
+        freq2[s2[i - n] - 'a']--;
+
+        if (freq1 == freq2) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+
+    string s1, s2;
+
+    cout << "Enter s1: ";
+    cin >> s1;
+
+    cout << "Enter s2: ";
+    cin >> s2;
+
+    if (checkInclusion(s1, s2)) {
+        cout << "true";
+    } else {
+        cout << "false";
+    }
+
+    return 0;
+}
